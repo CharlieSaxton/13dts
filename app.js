d1cb6d02f1058f2e7b89e97d9a206ea861a1db99
CharlieSaxton <63372478+CharlieSaxton@users.noreply.github.com>
2021-08-23 12:27:14 +1200

    more logging

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+console.log(process.env.DATABASE_URL);
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-console.log(process.env.DATABASE_URL);
+
 
 var app = express();
 
